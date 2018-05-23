a4fb5adbe34989a90e138b9dabf5a34998797c75
ysugimura <ysugimura@spiderman>
2018-05-23 12:14:44 +0900

    修正

--- src/com/cm55/recLucene/RlTable.java
+++ src/com/cm55/recLucene/RlTable.java
@@ -2,19 +2,40 @@ package com.cm55.recLucene;
 
 import java.lang.reflect.*;
 import java.util.*;
+import java.util.stream.*;
 
 import org.apache.lucene.document.*;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.index.*;
 
 /**
- * アノテーション付きのJavaクラスを元にテーブルが作成される。
- * <ul>
- * <li>すべてのフィールドはString型のみとする。
- * <li>Javaの直列化と同様にstatic,transientは無視される。
- * <li>ただ一つのプライマリキーフィールドが存在しなくてはならない。これは{@link RlFieldAttr}で指定される。
- * </ul>
- * 
+ * Luceneの{@link Document}とJavaクラスのマッピングオブジェクト
+ * <p>
+ * Luceneでの、いわゆるレコード、内部的には{@link Document}だが、これは、任意のフィールド名の任意の組み合わせ
+ * のものがいつでも書き込めてしまう。
+ * これでは扱いにくいので、ある程度の制限をかける。通常のORマッピングのように、Java側のクラスにあるフィールド名と
+ * その値のみを{@link Document}として書き込む。
+ * この目的のため、{@link RlTable}では、一つのJavaクラスを指定して、そのstatic/transientでないフィールドを
+ * 拾い上げ書き込み・読み込み対象とする。
+ * </p>
+ * <p>
+ * さらに、Luceneでは、そのままでは全く同じレコードを複数書き込んでしまうことができる。これを避けるため、Javaクラスの
+ * フィールドにプライマリキー指定をつけ、常にその値が唯一になるように書き込みを行うようにさせる。必要なければ、
+ * プライマリキーを非指定も可能、
+ * </p>
+ * <p>
+ * 具体的なJavaクラス側のフィールド指定については{@link RlField}に詳細がある。
+ * </p>
+ * <p>
+ * また、ごく稀にだが、動的にフィールドを決めたい場合もある。この場合にはJavaクラスを指定せずに、動的に作成した
+ * {@link RlField}の配列を指定することもできる。
+ * </p>
+ * <p>
+ * 注意点としては、このラッパーライブラリでは、一つのLuceneデータベース内において、一つのフィールド名は唯一でなければならないこと。
+ * もともとLuceneデータベースでは、「任意のフィールドの任意の組み合わせ」が可能であるため、この制限が無いと、RDB的に言えば、
+ * 「あちらのテーブルのフィールドと、こちらのテーブルのフィールドが一つのレコードとして格納されてしまいかねない」ためだ。
+ * これについては、一つの{@link RlDatabase}に格納可能なテーブルのセット{@link RlTableSet}を参照のこと。
+ * </p>
  * @author ysugimura
  */
 public class RlTable {
@@ -30,51 +51,49 @@ public class RlTable {
 
   /**
    * クラスを指定してマッピングを作成する
-   * 
-   * @param clazz
-   *          マッピング対象クラス
+   * @param clazz マッピング対象クラス
    */
   public RlTable(Class<?> recordClass) {
 
     this.recordClass = recordClass;
 
-    List<RlField> fieldsFromClass = new ArrayList<RlField>();
-
-    // このクラスで宣言されたすべてのフィールドを調査する。ただし、static/transientは除く
-    Arrays.stream(recordClass.getDeclaredFields()).forEach(javaField -> {
-
-      // フィールドモディファイアの調査。transient,staticを無視する。
-      {
+    // このクラスで宣言されたすべてのフィールドを取得する。static/transientを除く
+    List<RlField>fieldsFromClass = Arrays.stream(recordClass.getDeclaredFields())
+      .filter(javaField-> {
         int mod = javaField.getModifiers();
         if (Modifier.isTransient(mod))
-          return;
+          return false;
         if (Modifier.isStatic(mod))
-          return;
-      }
-
-      // このフィールドについてのRlFieldを作成して追加
-      fieldsFromClass.add(new RlField(javaField));
-    });
+          return false;
+        return true;
+      })
+      .map(javaField->new RlField(javaField))
+      .collect(Collectors.toList());
 
     init(fieldsFromClass.toArray(new RlField[0]));
   }
 
   /**
-   * フィールド配列を内部に格納する。プライマリキー重複をチェックする
+   * フィールドを指定する
    * 
-   * @param fields
+   * @param fields フィールド指定
    * @return
    */
   public RlTable(RlField... fields) {
     init(fields);
   }
 
+  /**
+   * フィールドリストを指定する
+   * @param fields フィールド指定
+   */
   public RlTable(List<RlField> fields) {
     init(fields.toArray(new RlField[0]));
   }
 
+  /** 作成時の初期化 */
   private void init(RlField... fields) {
-    // フィールド配列を内部に格納する
+    
     fieldMap = new HashMap<String, RlField>();
     for (RlField field : fields) {
 
@@ -94,12 +113,6 @@ public class RlTable {
         pkField = field;
       }
     }
-
-    // プライマリキーが指定されていなければならない。
-    // ※指定がなくてもいいことにした
-    /*
-     * if (pkField == null) { throw new RlException("プライマリキー指定がありません"); }
-     */
   }
 
   /**
@@ -137,8 +150,7 @@ public class RlTable {
   /**
    * フィールド名称から対応する{@link RlField}を取得する。存在しない場合はnullを返す。
    * 
-   * @param fieldName
-   *          フィールド名称
+   * @param fieldName フィールド名称
    * @return {@link RlField}
    */
   public RlField getFieldByName(String fieldName) {
@@ -146,8 +158,7 @@ public class RlTable {
   }
 
   /**
-   * 指定されたオブジェクトのプライマリキー{@link Term}を取得する
-   * 
+   * 指定されたオブジェクトのプライマリキー{@link Term}を取得する。
    * @param rec
    * @return
    */
@@ -176,8 +187,7 @@ public class RlTable {
   /**
    * 指定されたオブジェクトの内容から{@link Document}を作成する
    * 
-   * @param object
-   *          オブジェクト。クラスは{@link #recordClass}であること
+   * @param object オブジェクト。クラスは{@link #recordClass}であること
    * @return luceneの{@link Document}
    */
   public <T> Document getDocument(T object) {
