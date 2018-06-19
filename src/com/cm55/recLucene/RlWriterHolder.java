33dc70d4b73d4a822c15b2441e486a279d768653
ysugimura <ysugimura@spiderman>
2018-06-19 15:22:56 +0900

    修正

--- src/com/cm55/recLucene/RlWriterHolder.java
+++ src/com/cm55/recLucene/RlWriterHolder.java
@@ -5,29 +5,44 @@ import org.apache.lucene.index.*;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.*;
 
+/**
+ * Luceneの{@link IndexWriter}と{@link SearchManager}を保持する。
+ * データベースがオープンしているあいだは、これらがクローズされることはない。
+ * resetによってデータベース変更がされるときのみに、クローズされてから再オープンされる。
+ * @author ysugimura
+ *
+ */
 public class RlWriterHolder {
 
   private Directory directory;
   private RlTableSet tableSet;
   private IndexWriter indexWriter;
   private SearcherManager searcherManager;
-  
+
+  /**
+   * データベースディレクトリと、その中のテーブル定義を指定してリセットする。
+   * @param directory ディレクトリ
+   * @param tableSet テーブル定義集合
+   */
   public synchronized void reset(Directory directory, RlTableSet tableSet) {
     close();
     this.directory = directory;
     this.tableSet = tableSet;
   }
-  
+
+  /** Luceneの{@link IndexWriter}を取得する。これはオープン中データベースにおける唯一のものになる。 */
   public IndexWriter getIndexWriter() {
     ensure();
     return indexWriter;
   }
   
+  /** Luceneの{@link SearchManager}を取得する。これはオープン中データベースにおける唯一のものになる。 */
   public SearcherManager getSearcherManager() {
     ensure();
     return searcherManager;
   }
-  
+
+  /** 未作成であれば{@link IndexWriter}と{@link SearchManager}を作成する */
   private synchronized void ensure() {
     if (indexWriter != null) return;
     
@@ -51,6 +66,11 @@ public class RlWriterHolder {
     }
   }
   
+  /** 
+   * クローズする。
+   * データベースオープン中に呼び出されることは無い。
+   * 上位の{@link RlWriter}においてクローズを行っても、このメソッドが呼び出されることはない。
+   */
   public synchronized void close() {
     if (indexWriter == null) return;
     try {     
@@ -60,6 +80,9 @@ public class RlWriterHolder {
     }
     try {
       indexWriter.close();
+    } catch (java.nio.file.AccessDeniedException ex) {
+      // そもそもフォルダに何も無い場合はこの例外が発生する。"write.lock"にアクセスできないという。
+      // 無視する
     } catch (Exception ex) {
       throw new RlException(ex);
     }
